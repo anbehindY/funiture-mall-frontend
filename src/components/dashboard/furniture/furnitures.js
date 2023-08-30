bbecd22b2386556ca2e0f082d916977688588a3b
prius2055 <prius2055@gmail.com>
2023-08-30 13:40:42 +0100

    Include paragraph and header in furnitures page

--- src/components/dashboard/furniture/furnitures.js
+++ src/components/dashboard/furniture/furnitures.js
@@ -9,12 +9,33 @@ const Furnitures = () => (
     <div className="furniture-content">
       <div className="furniture-image">
         <img src={fimage} alt="Furniture" />
+        <div>
+          <h2>Furniture 1</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            nec mattis tellus. Ut semper dui eget efficitur sollicitudin.
+          </p>
+        </div>
       </div>
       <div className="furniture-image">
         <img src={fimage} alt="Furniture" />
+        <div>
+          <h2>Furniture 2</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            nec mattis tellus. Ut semper dui eget efficitur sollicitudin.
+          </p>
+        </div>
       </div>
       <div className="furniture-image">
         <img src={fimage} alt="Furniture" />
+        <div>
+          <h2>Furniture 3</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            nec mattis tellus. Ut semper dui eget efficitur sollicitudin.
+          </p>
+        </div>
       </div>
     </div>
   </div>
