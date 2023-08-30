09f7a1abd63f9c2becef50a553f825bf56cd8e36
prius2055 <prius2055@gmail.com>
2023-08-30 14:03:48 +0100

    Install redux with axios

--- src/store/furnitureSlice.js
+++ src/store/furnitureSlice.js
@@ -1,9 +1,11 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
 const furnitureSlice = createSlice({
   name: 'furniture',
   initialState,
   reducers: {},
   extraReducers: () => {},
 });
-
 
 export default furnitureSlice.reducer;
