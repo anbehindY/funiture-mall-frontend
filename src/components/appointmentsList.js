bd553d44f234e7bb1f5d1bb44c7c77785465e497
Esther Udondian <eudondian@gmail.com>
2023-08-30 13:47:15 +0100

    Add useEffect for fetching appointments data

--- src/components/appointmentsList.js
+++ src/components/appointmentsList.js
@@ -2,4 +2,6 @@ import React, { useEffect, useState } from 'react';
 
 function AppointmentsList() {
     const [appointments, setAppointments] = useState([]);
-  
+    useEffect(() => {
+        async function fetchAppointments() {
+          try 
