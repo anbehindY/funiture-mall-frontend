0e8961b1d3654b0fb1c5df6568bc812eec27903c
Esther Udondian <eudondian@gmail.com>
2023-09-01 15:58:23 +0100

    Integrate Date Input for Selecting Appointment Date

--- src/components/reserved.js
+++ src/components/reserved.js
@@ -20,3 +20,20 @@ function Reserve() {
                 type="text"
                 value={username}
                 onChange={event => setUsername(event.target.value)}
+                />
+        </label>
+        <label>
+          Selected Item:
+          <input
+            type="text"
+            value={selectedItem}
+            onChange={event => setSelectedItem(event.target.value)}
+          />
+          </label>
+        <label>
+          Select Date:
+          <input
+            type="date"
+            value={selectedDate}
+            onChange={event => setSelectedDate(event.target.value)}
+          />
