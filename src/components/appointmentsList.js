e89b4ceb81e644d508a618afce1ccecc2352615b
Esther Udondian <eudondian@gmail.com>
2023-09-02 00:03:27 +0100

    Implement AppointmentsList component with Redux integration

--- src/components/appointmentsList.js
+++ src/components/appointmentsList.js
@@ -1,3 +1,8 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAppointments } from '../store/appointmentSlice'; // Replace with the actual import path
+
+function AppointmentsList() {
+  const appointments = useSelector((state) => state.appointments);
+    const error = useSelector((state) => state.appointments.error);
+  const dispatch = useDispatch();
