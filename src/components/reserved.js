54e23e5423449652ebc7e44b02c8ce36ff752030
Esther Udondian <eudondian@gmail.com>
2023-09-01 15:55:17 +0100

    Create Reserve Appointment Form and Manage Username Input

--- src/components/reserved.js
+++ src/components/reserved.js
@@ -9,3 +9,14 @@ function Reserve() {
     const handleReserve = () => {
       // Handle the reservation process, e.g., send data to the server
     };
+
+    return (
+        <div>
+          <h2>Reserve an Appointment</h2>
+          <form onSubmit={handleReserve}>
+            <label>
+              Username:
+              <input
+                type="text"
+                value={username}
+                onChange={event => setUsername(event.target.value)}
