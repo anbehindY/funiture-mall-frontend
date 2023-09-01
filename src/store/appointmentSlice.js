8dbfcc0e77283d3c558a9c70e6a58874b7583a80
Esther Udondian <eudondian@gmail.com>
2023-09-02 00:17:00 +0100

    Add async thunk for fetching appointments from the server

--- src/store/appointmentSlice.js
+++ src/store/appointmentSlice.js
@@ -5,3 +5,10 @@ const initialState = {
   appointments: [],
   error: null,
 };
+/ Create an async thunk for fetching appointments
+export const fetchAppointments = createAsyncThunk('appointments/fetchAppointments', async () => {
+  try {
+    const response = await fetch('http://localhost:3001/api/v1/appointments');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
