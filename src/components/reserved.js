f80d66dcec88ae6780585328d9ef97a1e2b93c9d
Esther Udondian <eudondian@gmail.com>
2023-09-01 16:24:13 +0100

    Initialize Redux State with Default Values

--- src/components/reserved.js
+++ src/components/reserved.js
@@ -2,49 +2,53 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { setUsername } from '../redux/reservationActions';
 
-   return (
-        <div>
-          <h2>Reserve an Appointment</h2>
-          <form onSubmit={handleReserve}>
-            <label>
-              Username:
-              <input
-                type="text"
-                value={username}
-                onChange={event => setUsername(event.target.value)}
-                />
+function Reserve({ username, setUsername }) {
+  return (
+    <div>
+      <h2>Reserve an Appointment</h2>
+      <form onSubmit={handleReserve}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
         </label>
         <label>
           Selected Item:
           <input
             type="text"
             value={selectedItem}
-            onChange={event => setSelectedItem(event.target.value)}
+            onChange={(event) => setSelectedItem(event.target.value)}
           />
-          </label>
+        </label>
         <label>
           Select Date:
           <input
             type="date"
             value={selectedDate}
-            onChange={event => setSelectedDate(event.target.value)}
+            onChange={(event) => setSelectedDate(event.target.value)}
           />
-          </label>
+        </label>
         <label>
           Select City:
           <input
             type="text"
             value={selectedCity}
-            onChange={event => setSelectedCity(event.target.value)}
+            onChange={(event) => setSelectedCity(event.target.value)}
           />
-          </label>
+        </label>
         <button type="submit">Reserve</button>
       </form>
     </div>
   );
-  const mapStateToProps = (state) => ({
-    username: state.username,
-  });
-  
+}
+const mapStateToProps = (state) => ({
+  username: state.username,
+});
+const mapDispatchToProps = {
+  setUsername,
+};
 
-export default Reserve;
+export default connect(mapStateToProps, mapDispatchToProps)(Reserve);
---
+++ src/store/reservationReducer.js
@@ -0,0 +1,6 @@
+const initialState = {
+    username: '',
+    selectedItem: '',
+    selectedDate: '',
+    selectedCity: '',
+  };
