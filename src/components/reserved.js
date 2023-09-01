44c58d8fd2151de398ccae8a427a10e9b4d2d27f
Esther Udondian <eudondian@gmail.com>
2023-09-01 15:52:50 +0100

    Implement Reserve component with state management

--- src/components/reserved.js
+++ src/components/reserved.js
@@ -1,1 +1,11 @@
 import React, { useState } from 'react';
+
+function Reserve() {
+    const [username, setUsername] = useState(''); // Set the username if available
+    const [selectedItem, setSelectedItem] = useState(''); // Autofill the selected item
+    const [selectedDate, setSelectedDate] = useState('');
+    const [selectedCity, setSelectedCity] = useState('');
+  
+    const handleReserve = () => {
+      // Handle the reservation process, e.g., send data to the server
+    };
