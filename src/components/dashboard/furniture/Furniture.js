575046929273d251458cc29d67f0fc0d617cef06
OsmanFP <osmanwako411@gmail.com>
2023-09-07 17:17:24 +0300

    Fix eslint error

--- src/components/dashboard/furniture/Furniture.js
+++ src/components/dashboard/furniture/Furniture.js
@@ -17,44 +17,51 @@ const Furniture = () => {
   const filteredItem = furnitures[0]?.filter((fur) => fur.id === Number(id));
 
   return (
-<div className="main_furniture_select"> <LeftSidebar />
-<div className="furniture_select">
+    <div className="main_furniture_select">
+      {' '}
+      <LeftSidebar />
+      <div className="furniture_select">
 
-  {filteredItem && (
-    <div className="furniture-image">
-      <img src={`${filteredItem[0].image}`} className="imgfurniture_select" alt="Furniture" />
-      <h1 className="name_furniture_select">{filteredItem[0].name}</h1>
-      <p  className="description_furniture_select">{filteredItem[0].description}</p>
-      <p  className="price_furniture_select">
-        <span >Price:</span>
-        <span>{filteredItem[0].price}</span>
-      </p>
-      <p className="warranty_furniture_select">
-        <span >Waranty:</span>
-        <span>
-          {filteredItem[0].warranty}
-          years
-        </span>
-      </p>
-      <p className="production_furniture_select">
-        <span >Production date:</span>
-        <span>{filteredItem[0].created_at}</span>
-      </p>
-      <Link to="/new-appointment" state={{ furniture: filteredItem[0] }}>
-      <button type="submit" className="reserver_furniture"> Book appointment <i className="fa fa-arrow-circle-o-right" /></button>
-      </Link>
-      
-      <Link to="/dashboard">
-      <button type="submit" className="back_furniture"> <i className="fa fa-arrow-left" /> </button>
-      </Link>
+        {filteredItem && (
+        <div className="furniture-image">
+          <img src={`${filteredItem[0].image}`} className="imgfurniture_select" alt="Furniture" />
+          <h1 className="name_furniture_select">{filteredItem[0].name}</h1>
+          <p className="description_furniture_select">{filteredItem[0].description}</p>
+          <p className="price_furniture_select">
+            <span>Price:</span>
+            <span>{filteredItem[0].price}</span>
+          </p>
+          <p className="warranty_furniture_select">
+            <span>Waranty:</span>
+            <span>
+              {filteredItem[0].warranty}
+              years
+            </span>
+          </p>
+          <p className="production_furniture_select">
+            <span>Production date:</span>
+            <span>{filteredItem[0].created_at}</span>
+          </p>
+          <Link to="/new-appointment" state={{ furniture: filteredItem[0] }}>
+            <button type="submit" className="reserver_furniture">
+              {' '}
+              Book appointment
+              <i className="fa fa-arrow-circle-o-right" />
+            </button>
+          </Link>
 
-    
-           
-       
+          <Link to="/dashboard">
+            <button type="submit" className="back_furniture">
+              {' '}
+              <i className="fa fa-arrow-left" />
+              {' '}
+            </button>
+          </Link>
+
+        </div>
+        )}
+      </div>
     </div>
-  )}
-</div>
-</div>
   );
 };
 export default Furniture;
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -58,52 +58,56 @@ const Login = () => {
 
   return (
     <>
-    <div className='blur'></div>
-    <div className="container-login">
-      <div className="container-form">
-        <form className="form-login" onSubmit={loginHandler}>
-          <h2>Login Furniture Mall</h2>
-          <div className="form-row">
-            <input
-              type="text"
-              name="email"
-              id="email"
-              className="input-text"
-              placeholder="Enter email address"
-              required
-              onChange={handleChange}
-              value={userData.username}
-            />
+      <div className="blur" />
+      <div className="container-login">
+        <div className="container-form">
+          <form className="form-login" onSubmit={loginHandler}>
+            <h2>Login Furniture Mall</h2>
+            <div className="form-row">
+              <input
+                type="text"
+                name="email"
+                id="email"
+                className="input-text"
+                placeholder="Enter email address"
+                required
+                onChange={handleChange}
+                value={userData.username}
+              />
+            </div>
+            <div className="form-row">
+              <input
+                type="password"
+                name="password"
+                id="password_id"
+                className="input-text"
+                placeholder="Enter Password"
+                required
+                minLength={6}
+                maxLength={50}
+                onChange={handleChange}
+                value={userData.password}
+              />
+            </div>
+            <div className="form-row-last">
+              <input
+                type="submit"
+                name="register"
+                className="register"
+                value="Login"
+              />
+            </div>
+          </form>
+          <div className="P_singin">
+            Don&apos;t have an account?
+            <Link to="/signup" className="loginlink">
+              {' '}
+              <i className="fa fa-arrow-right" />
+              Click here to Register
+            </Link>
           </div>
-          <div className="form-row">
-            <input
-              type="password"
-              name="password"
-              id="password_id"
-              className="input-text"
-              placeholder="Enter Password"
-              required
-              minLength={6}
-              maxLength={50}
-              onChange={handleChange}
-              value={userData.password}
-            />
-          </div>
-          <div className="form-row-last">
-            <input
-              type="submit"
-              name="register"
-              className="register"
-              value="Login"
-            />
-          </div>
-        </form>
-        <div className='P_singin'>
-          Don&apos;t have an account?
-          <Link to="/signup" className='loginlink'> <i className="fa fa-arrow-right" />Click here to Register</Link>
         </div>
       </div>
-    </div>
     </>
   );
 };
