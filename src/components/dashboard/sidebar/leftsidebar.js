6b95d560804d7ef879717a5286cbb87b02555992
OsmanFP <osmanwako411@gmail.com>
2023-09-07 16:57:55 +0300

    Update link url

--- src/components/dashboard/sidebar/leftsidebar.js
+++ src/components/dashboard/sidebar/leftsidebar.js
@@ -1,57 +1,54 @@
 import { NavLink } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import Logout from '../../login/Logout';
 import logo from './logo.png';
 
-const LeftSidebar = ({ setCurrUser }) => (
-  <div className="leftsidebar-container">
-    <div className="upper-part">
-      <div className="app-logo">
-        <div className="logo-image">
-          <img src={logo} alt="Application Logo" />
-          <h3>Dashboard</h3>
+const LeftSidebar = (data) => {
+  const { setCurrUser } = data;
+  return (
+    <div className="leftsidebar-container">
+      <div className="upper-part">
+        <div className="app-logo">
+          <div className="logo-image">
+            <img src={logo} alt="Application Logo" />
+            <h3>Dashboard</h3>
+          </div>
         </div>
       </div>
+      <div className="left-content">
+        <ul className="action-list">
+          <li className="item">
+            <NavLink to="/furnitures" className="menubar-link">
+              <span>Furnitures</span>
+            </NavLink>
+          </li>
+          <li className="item">
+            <NavLink to="/my-appointments" className="menubar-link">
+              <span> My Appointments</span>
+            </NavLink>
+          </li>
+          <li className="item">
+            <NavLink to="/new-appointment" className="menubar-link">
+              <span>Add Appointment</span>
+            </NavLink>
+          </li>
+          <li className="item">
+            <NavLink to="/new-furniture" className="menubar-link">
+              <span>Add Furniture</span>
+            </NavLink>
+          </li>
+          <li className="item">
+            <NavLink to="/delete-furniture" className="menubar-link">
+              <span>Delete Furniture</span>
+            </NavLink>
+          </li>
+        </ul>
+        <ul className="category-list">
+          <li className="item">
+            <Logout setCurrUser={setCurrUser} />
+          </li>
+        </ul>
+      </div>
     </div>
-    <div className="left-content">
-      <ul className="action-list">
-        <li className="item">
-          <NavLink to="/furnitures" className="menubar-link">
-            <span>Furnitures</span>
-          </NavLink>
-        </li>
-        <li className="item">
-          <NavLink to="/my-appointments" className="menubar-link">
-            <span> My Appointments</span>
-          </NavLink>
-        </li>
-        <li className="item">
-          <NavLink to="/new-appointment" className="menubar-link">
-            <span>Add Appointment</span>
-          </NavLink>
-        </li>
-        <li className="item">
-          <NavLink to="/products" className="menubar-link">
-            <span>Add Furniture</span>
-          </NavLink>
-        </li>
-        <li className="item">
-          <NavLink to="/histories" className="menubar-link">
-            <span>Delete Furniture</span>
-          </NavLink>
-        </li>
-      </ul>
-      <ul className="category-list">
-        <li className="item">
-          <Logout setCurrUser={setCurrUser} />
-        </li>
-      </ul>
-    </div>
-  </div>
-);
-
-export default LeftSidebar;
-
-LeftSidebar.propTypes = {
-  setCurrUser: PropTypes.func.isRequired,
+  );
 };
+export default LeftSidebar;
